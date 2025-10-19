68f4a358d5ad5f1f393b1aa03ce898fd95f7d38a
yousaf_1004564 <nayousaf@gmail.com>
2025-10-19 14:09:17 -0700

    updated version

--- main.cc
+++ main.cc
@@ -10,22 +10,26 @@ void die() {
 	exit(0);
 }
 
-int main ()
-{
+int main() {
 	//But on one line instead of three, because I'm cool like that
 	string filename = read("Please enter a filename:\n");
+//	cin >> filename;
 	ifstream case_file(filename);
 	if (!case_file) die();
 
-	size_t n = 0;
-	if (n < 2) die();
 	vector<int> case_name;
 	vector<int> choices;
-	int c = 0;
 
+	int userInput = 0;
+	cout << "Please enter a briefcase to open" << endl;
+	while (true) {
+		cin >> userInput;
+		vec.push_back(case_name)
+		if (userInput == -1) break;
+	}
 	//Next...
 	//Do you remember how to open a file? check
-	//How to check to see if the file opened successfully?
+	//How to check to see if the file opened successfully? Check
 	//How do you store data in a vector?
 	//How do you average values across a vector?
 }
