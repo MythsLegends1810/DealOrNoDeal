13c19cdb77b94187388618053a87f52302e1d526
yousaf_1004564 <nayousaf@gmail.com>
2025-10-19 17:33:08 -0700

    updated version

--- main.cc
+++ main.cc
@@ -17,19 +17,29 @@ int main() {
 	ifstream case_file(filename);
 	if (!case_file) die();
 
+	int N = 0;;
+	case_file >> N;
 	vector<int> case_name;
 	vector<int> choices;
 
-	int userInput = 0;
+
+	for (int i = 0; i < N; i++) {
+		int userInput = 0;
+		case_file >> userInput;
+		case_name.push_back(userInput);
+	}
+	int userVal = 0;
 	cout << "Please enter a briefcase to open" << endl;
 	while (true) {
-		cin >> userInput;
-		vec.push_back(case_name)
-		if (userInput == -1) break;
+		cin >> userVal;
+		//	for (int i = 0; i < case_name.size(); i++)
+		if (userVal > 0 && userVal <= case_name.size())
+			cout << "that case contained " << case_name.at() << endl;
+		if (userVal == -1) break;
 	}
-	//Next...
-	//Do you remember how to open a file? check
-	//How to check to see if the file opened successfully? Check
-	//How do you store data in a vector?
-	//How do you average values across a vector?
+//Next...
+//Do you remember how to open a file? check
+//How to check to see if the file opened successfully? Check
+//How do you store data in a vector?
+//How do you average values across a vector?
 }
